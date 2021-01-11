b92f4cc23264be7da1f7d843659270bb2cdaa3df
kirin123kirin <kirin123kirin@gmail.com>
2021-01-12 00:01:04 +0900

    stable. bugfixed

--- qmk/rev1/keymaps/default/keymap.c
+++ qmk/rev1/keymaps/default/keymap.c
@@ -33,9 +33,9 @@ enum layers{
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT(
     KC_ESC,               KC_F1,    KC_F2,     KC_F3,     KC_F4,           KC_F5,      KC_F6,     KC_F7,      KC_F8,                KC_F9,     KC_F10,    KC_F11,     KC_F12,      KC_PSCR,       KC_SCROLLLOCK,   KC_PAUSE,
-    KC_GRV,     JP_1,     JP_2,     JP_3,      JP_4,      JP_5,            JP_6,       JP_7,      JP_8,       JP_9,      KC_0,      JP_MINS,   JP_CIRC,   JP_YEN,     KC_BSPACE,   KC_INSERT,     KC_HOME,         KC_PGUP,
+    JP_ZKHK,    JP_1,     JP_2,     JP_3,      JP_4,      JP_5,            JP_6,       JP_7,      JP_8,       JP_9,      KC_0,      JP_MINS,   JP_CIRC,   JP_YEN,     KC_BSPACE,   KC_INSERT,     KC_HOME,         KC_PGUP,
     KC_TAB,     KC_Q,     KC_W,     KC_E,      KC_R,      KC_T,            KC_Y,       KC_U,      KC_I,       KC_O,      KC_P,      JP_AT,     JP_LBRC,                            KC_DELETE,     KC_END,          KC_PGDOWN,
-    KC_CAPSLOCK,KC_A,     KC_S,     KC_D,      KC_F,      KC_G,            KC_H,       KC_J,      KC_K,       KC_L,      JP_SCLN,   JP_COLN,   JP_RBRC,   KC_ENTER,
+    KC_CAPS,    KC_A,     KC_S,     KC_D,      KC_F,      KC_G,            KC_H,       KC_J,      KC_K,       KC_L,      JP_SCLN,   JP_COLN,   JP_RBRC,   KC_ENTER,
     KC_LSHIFT,            KC_Z,     KC_X,      KC_C,      KC_V,            KC_B,       KC_N,      KC_M,       JP_COMM,   JP_DOT,    JP_SLSH,   JP_BSLS,   KC_RSHIFT,               KC_WWW_BACK,   KC_UP,           KC_WWW_FORWARD,
     KC_LCTRL,   KC_LWIN,            KC_LALT,   JP_MHEN,   KC_SPACE,        KC_SPACE,   JP_HENK,   KC_RCTRL,              KC_RALT,   KC_RWIN,   KC_MENU,   KC_RCTRL,                KC_LEFT,       KC_DOWN,         KC_RIGHT
   )
